43500bace3972b1b0c07fa30a981546743ea4b6f
Thomas Madden <tmadden@gmail.com>
2023-04-06 14:40:18 -0400

    Add other libs to config

--- ports/skia/unofficial-skia-config.cmake
+++ ports/skia/unofficial-skia-config.cmake
@@ -26,53 +26,62 @@ if(NOT TARGET unofficial::skia::skia)
         set(z_vcpkg_skia_root "")
     endif()
 
-    add_library(unofficial::skia::skia UNKNOWN IMPORTED)
-    set_target_properties(unofficial::skia::skia PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${z_vcpkg_skia_root}/include/skia"
-    )
+    function(z_vcpkg_skia_find_and_add_library name)
+        add_library(unofficial::skia::${name} UNKNOWN IMPORTED)
+        set_target_properties(unofficial::skia::${name} PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${z_vcpkg_skia_root}/include/skia"
+        )
 
-    find_library(z_vcpkg_skia_lib_release NAMES skia skia.dll PATHS "${z_vcpkg_skia_root}/lib" NO_DEFAULT_PATH)
-    find_library(z_vcpkg_skia_lib_debug NAMES skia skia.dll PATHS "${z_vcpkg_skia_root}/debug/lib" NO_DEFAULT_PATH)
-    mark_as_advanced(z_vcpkg_skia_lib_release z_vcpkg_skia_lib_debug)
+        find_library(z_vcpkg_skia_lib_release NAMES ${name} ${name}.dll PATHS "${z_vcpkg_skia_root}/lib" NO_DEFAULT_PATH)
+        find_library(z_vcpkg_skia_lib_debug NAMES ${name} ${name}.dll PATHS "${z_vcpkg_skia_root}/debug/lib" NO_DEFAULT_PATH)
+        mark_as_advanced(z_vcpkg_skia_lib_release z_vcpkg_skia_lib_debug)
 
-    set(z_vcpkg_skia_config_debug "\$<CONFIG:Debug>")
-    if(NOT z_vcpkg_skia_lib_debug)
-        set(z_vcpkg_skia_config_debug "0")
-    endif()
+        set(z_vcpkg_skia_config_debug "\$<CONFIG:Debug>")
+        if(NOT z_vcpkg_skia_lib_debug)
+            set(z_vcpkg_skia_config_debug "0")
+        endif()
 
-    z_vcpkg_skia_get_link_libraries(
-        z_vcpkg_skia_link_libs_release
-        "${z_vcpkg_skia_root}/lib;${z_vcpkg_skia_root}/debug/lib"
-        "@SKIA_DEP_REL@"
-    )
-    set_target_properties(unofficial::skia::skia PROPERTIES
-        IMPORTED_CONFIGURATIONS RELEASE
-        IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
-        IMPORTED_LOCATION_RELEASE "${z_vcpkg_skia_lib_release}"
-        INTERFACE_COMPILE_DEFINITIONS "\$<\$<NOT:${z_vcpkg_skia_config_debug}>:@SKIA_DEFINITIONS_REL@>"
-        INTERFACE_LINK_LIBRARIES "\$<LINK_ONLY:\$<\$<NOT:${z_vcpkg_skia_config_debug}>:${z_vcpkg_skia_link_libs_release}>>"
-    )
-
-    if(z_vcpkg_skia_lib_debug)
         z_vcpkg_skia_get_link_libraries(
-            z_vcpkg_skia_link_libs_debug
-            "${z_vcpkg_skia_root}/debug/lib;${z_vcpkg_skia_root}/lib"
-            "@SKIA_DEP_DBG@"
+            z_vcpkg_skia_link_libs_release
+            "${z_vcpkg_skia_root}/lib;${z_vcpkg_skia_root}/debug/lib"
+            "@SKIA_DEP_REL@"
         )
-        set_property(TARGET unofficial::skia::skia APPEND PROPERTY
-            IMPORTED_CONFIGURATIONS DEBUG
+        set_target_properties(unofficial::skia::${name} PROPERTIES
+            IMPORTED_CONFIGURATIONS RELEASE
+            IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+            IMPORTED_LOCATION_RELEASE "${z_vcpkg_skia_lib_release}"
+            INTERFACE_COMPILE_DEFINITIONS "\$<\$<NOT:${z_vcpkg_skia_config_debug}>:@SKIA_DEFINITIONS_REL@>"
+            INTERFACE_LINK_LIBRARIES "\$<LINK_ONLY:\$<\$<NOT:${z_vcpkg_skia_config_debug}>:${z_vcpkg_skia_link_libs_release}>>"
         )
-        set_target_properties(unofficial::skia::skia PROPERTIES
-            IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
-            IMPORTED_LOCATION_DEBUG "${z_vcpkg_skia_lib_debug}"
-        )
-        set_property(TARGET unofficial::skia::skia APPEND PROPERTY
-            INTERFACE_COMPILE_DEFINITIONS "\$<\$<CONFIG:Debug>:@SKIA_DEFINITIONS_DBG@>"
-        )
-        set_property(TARGET unofficial::skia::skia APPEND PROPERTY
-            INTERFACE_LINK_LIBRARIES "\$<LINK_ONLY:\$<\$<CONFIG:Debug>:${z_vcpkg_skia_link_libs_debug}>>"
-        )
-    endif()
+
+        if(z_vcpkg_skia_lib_debug)
+            z_vcpkg_skia_get_link_libraries(
+                z_vcpkg_skia_link_libs_debug
+                "${z_vcpkg_skia_root}/debug/lib;${z_vcpkg_skia_root}/lib"
+                "@SKIA_DEP_DBG@"
+            )
+            set_property(TARGET unofficial::skia::${name} APPEND PROPERTY
+                IMPORTED_CONFIGURATIONS DEBUG
+            )
+            set_target_properties(unofficial::skia::${name} PROPERTIES
+                IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+                IMPORTED_LOCATION_DEBUG "${z_vcpkg_skia_lib_debug}"
+            )
+            set_property(TARGET unofficial::skia::${name} APPEND PROPERTY
+                INTERFACE_COMPILE_DEFINITIONS "\$<\$<CONFIG:Debug>:@SKIA_DEFINITIONS_DBG@>"
+            )
+            set_property(TARGET unofficial::skia::${name} APPEND PROPERTY
+                INTERFACE_LINK_LIBRARIES "\$<LINK_ONLY:\$<\$<CONFIG:Debug>:${z_vcpkg_skia_link_libs_debug}>>"
+            )
+        endif()
+    endfunction()
+
+    z_vcpkg_skia_find_and_add_library(skia)
+    z_vcpkg_skia_find_and_add_library(skparagraph)
+    z_vcpkg_skia_find_and_add_library(skshaper)
+    z_vcpkg_skia_find_and_add_library(sktext)
+
+    unset(z_vcpkg_skia_find_and_add_library)
     unset(z_vcpkg_skia_config_debug)
     unset(z_vcpkg_skia_root)
 endif()
